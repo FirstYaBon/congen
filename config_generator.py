ae2353d04072c9c6af993960ba718ba22d4b6805
myfrozty <fah.frozty@gmail.com>
2017-06-28 13:03:23 +0700

    Add files via upload

--- config_generator.py
+++ config_generator.py
@@ -1,52 +1,57 @@
 import xlrd
 import xlwt
+import openpyxl
+from openpyxl import Workbook
+from openpyxl import load_workbook
+from xlutils.copy import copy
+from xlrd import open_workbook
 
-workbook = xlrd.open_workbook('test.xlsx')
+workbook = xlrd.open_workbook('config_file.xlsx')
 sheet = workbook.sheet_by_name('test')
 
-#first column
-indexArray = sheet.col_values(0)
-indexArray.remove('index')
+#first column: Index
+index_array = sheet.col_values(0)
+index_array.remove('index')
 
-#second column
-serviceArray = sheet.col_values(1)
-serviceArray.remove('Service_Set_(new)')
+#second column: Service_Set_(new)
+service_name_array = sheet.col_values(1)
+service_name_array.remove('Service_Set_(new)')
 
-#third column
-tcpArray = sheet.col_values(2)
-tcpArray.remove('TCP')
-for i in range(len(tcpArray)):
-    tcpArray[i] = tcpArray[i].split(',')
+#third column: TCP
+tcp_array = sheet.col_values(2)
+tcp_array.remove('TCP')
+for i in range(len(tcp_array)):
+    tcp_array[i] = tcp_array[i].split(',')
 
-#fourth column
-udpArray = sheet.col_values(3)
-udpArray.remove('UDP')
-for i in range(len(udpArray)):
-    udpArray[i] = udpArray[i].split(',')
+#fourth column: UDP
+udp_array = sheet.col_values(3)
+udp_array.remove('UDP')
+for i in range(len(udp_array)):
+    udp_array[i] = udp_array[i].split(',')
 
 
-#fifth column
-desArray = sheet.col_values(4)
-desArray.remove('description')
+#fifth column: description
+desc_array = sheet.col_values(4)
+desc_array.remove('description')
 
-size = len(indexArray)
+size = len(index_array)
+
+config_array = []
 
 for i in range(size):
-##    print(tcpArray[i])
-##    print(len(tcpArray[i]))
-    print("#")
-    if(tcpArray[i][0]==""):
+    config_array.append([])
+    config_array[i].append("#")
+    if(tcp_array[i][0]==""):
         protocol_type = "udp"
-        protocol_array = udpArray[i]
+        protocol_array = udp_array[i]
     else:
         protocol_type = "tcp"
-        protocol_array = tcpArray[i]
-    config = "ip service-set "+serviceArray[i]+" type object"
-    print(config)
-    if(desArray[i]!=""):
-        print("    description "+ desArray[i])
-
-##    print(protocol_type)
+        protocol_array = tcp_array[i]
+    config = "ip service-set "+service_name_array[i]+" type object"
+    config_array[i].append(config)
+    if(desc_array[i]!=""):
+        desc_line = "    description "+ desc_array[i]
+        config_array[i].append(desc_line)
 
     for j in range(len(protocol_array)):
         protocol_array[j]=str(protocol_array[j]).split("-")
@@ -54,5 +59,21 @@ for i in range(size):
             service_code = "    service "+str(j)+" protocol "+protocol_type+" destination-port "+protocol_array[j][0]+" to "+protocol_array[j][1]
         else:
             service_code = "    service "+str(j)+" protocol "+protocol_type+" destination-port "+protocol_array[j][0]
-        print(service_code)
-    print("#")
+        config_array[i].append(service_code)
+    config_array[i].append("#")
+
+file = "config_file_test.xlsx"
+workbook = load_workbook(filename = file)
+worksheet = workbook.get_sheet_by_name("test")
+print(len(config_array))
+for x in range(len(index_array)):
+    write_string = ''
+    for j in range(len(config_array[x])):
+        write_string = write_string + config_array[x][j]+"\n"
+    #print(write_string)
+##    worksheet.cell(row = x+1, column = 6, value = write_string)
+    worksheet['F1']='Configuration'
+    worksheet['F'+str(x+2)]=write_string
+    print(x+1)
+            
+workbook.save(filename = file)
