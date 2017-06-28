77378aa13bb9fa6270a50b99ad9a9e332cd6a828
Pongsakorn Chanchaipol <firstcardistry@gmail.com>
2017-06-28 08:46:36 +0700

    Add files via upload

--- config_generator.py
+++ config_generator.py
@@ -1,26 +1,55 @@
 import xlrd
 import xlwt
 
-workbook = xlrd.open_workbook('config_file.xlsx')
+workbook = xlrd.open_workbook('test.xlsx')
 sheet = workbook.sheet_by_name('test')
 
+#first column
 indexArray = sheet.col_values(0)
 indexArray.remove('index')
 
+#second column
 serviceArray = sheet.col_values(1)
 serviceArray.remove('Service_Set_(new)')
 
+#third column
 tcpArray = sheet.col_values(2)
 tcpArray.remove('TCP')
-tcpArray[0] = tcpArray[0].split(',')
+for i in range(len(tcpArray)):
+    tcpArray[i] = tcpArray[i].split(',')
+
+#fourth column
 udpArray = sheet.col_values(3)
 udpArray.remove('UDP')
+for i in range(len(udpArray)):
+    udpArray[i] = udpArray[i].split(',')
 
+
+#fifth column
 desArray = sheet.col_values(4)
 desArray.remove('description')
 
-for i in range (len(indexArray)):
-    indexArray[i] = i+1
+size = len(indexArray)
+
+for i in range(size):
+##    print(tcpArray[i])
+##    print(len(tcpArray[i]))
+    if(tcpArray[i][0]==""):
+        protocol_type = "udp"
+        protocol_array = udpArray[i]
+    else:
+        protocol_type = "tcp"
+        protocol_array = tcpArray[i]
+    config = "ip service-set "+serviceArray[i]+" type object description "+ desArray[i]
+    print(config)
+##    print(protocol_type)
+
+    for j in range(len(protocol_array)):
+        service_code = "    service "+str(j)+" protocol "+protocol_type+" destination-port "+str(protocol_array[j])
+        print(service_code)
+
+
+
 
 
 
